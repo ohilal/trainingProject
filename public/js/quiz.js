48dad0c9751c2ab2facfa35923a885f52e165d4a
ohilal <omniahilal@gmail.com>
2026-08-10 11:12:37 +0300

    quiz

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -7,6 +7,13 @@ $(document).ready(function () {
     }
     $("#btnQuestions .btnQuestion:first").addClass("process-step-active");
 
+    // Qwen 
+    //   $("#saveButton").click(function (event) {
+    //     event.preventDefault();
+    //     saveOnly();
+    // });
+
+
     $("#saveAndClose").click(function (event) {
         event.preventDefault();
 
@@ -83,4 +90,18 @@ function saveAndClose() {
     $.when.apply($, requests).then(function () {
         window.location.reload();
     });
-}
+    }
+
+// function saveOnly() {
+//     $(".workout_questions").each(function () {
+//         var data = $(this).serialize();
+//         var url = $(this).attr("action");
+
+//         $.post(url, data, function (data) {
+//             // Optionally handle individual response if needed
+//         });
+//     });
+
+//     // Show a success message or visual feedback
+//     alert("{{ __('Your answers have been saved successfully!') }}");
+// }
